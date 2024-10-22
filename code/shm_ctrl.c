2f3eb49c4219540fe4a4059c66cd41035653a769
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-22 15:25:42 +0000

    shm_open오류수정

--- code/shm_ctrl.c
+++ code/shm_ctrl.c
@@ -18,7 +18,7 @@ void* shm_setting(){
         fprintf(stderr, "E:ptr was already setted...\n");
         return ptr;
     }
-    fd=shm_open("SHM_NAME",O_RDWR|O_CREAT|O_TRUNC,"rw");
+    fd=shm_open("SHM_NAME",O_RDWR|O_CREAT|O_TRUNC,0666);
     if(fd<0){
         fprintf(stderr,"E:shm_setting failed\n");
         return NULL;
