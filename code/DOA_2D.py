d2c58b98b48adf8016ebb3d8c6806aa740d602d1
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-30 15:21:09 +0000

    test

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -25,12 +25,12 @@ stream = p.open(format=FORMAT,
 print("* 녹음을 시작합니다")
 
 frames = []
-
+test_frames=[]
 for _ in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
     data = stream.read(CHUNK)
     frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, CHANNELS))
+    test_frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, CHANNELS))
 
-test_frames=[0 for i in range(len(frames))]
 i=0
 max_db=0
 while True:
