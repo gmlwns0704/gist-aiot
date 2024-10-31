9b5e017c7f576234389182f7b9e97c59792de1b4
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 13:34:12 +0000

    test

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -5,6 +5,7 @@ import wave
 import time
 import sys
 import torch
+import threading
 
 sys.path.append('/home/rasp/venv/')
 sys.path.append('/home/rasp/venv/gist-aiot/')
@@ -29,7 +30,8 @@ class DOA_2D_listener():
                  min_volume=1500,
                  sound_pre_offset=0.3,
                  detect_callback=None,
-                 input_model=None):
+                 input_model=None,
+                 max_thread_num=3):
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = channels
         self.RATE = sr
@@ -41,6 +43,9 @@ class DOA_2D_listener():
             self.DETECT_CALLBACK=self.default_callback
         else:
             self.DETECT_CALLBACK=detect_callback
+        self.CALLBACK_THREAD_LIST=[]
+        for i in range(max_thread_num):
+            self.CALLBACK_THREAD_LIST.append(threading.Thread(target=self.DETECT_CALLBACK))
 
         # PyAudio 객체 생성
         self.PYAUDIO_INSTANCE = pyaudio.PyAudio()
@@ -68,15 +73,14 @@ class DOA_2D_listener():
     
     def start_detect(self):
         frames = []
-        test_frames=[]
+        self.test_frames=[]
         frame_len=(int(self.RATE / self.CHUNK * self.RECORD_SECONDS))
         for i in range(frame_len):
             data = self.STREAM.read(self.CHUNK, exception_on_overflow=False)
             frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS))
-            test_frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS))
+            self.test_frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS))
 
         print("* waiting for loud volume")
-        print(np.array(frames).shape)
         i=0
         while True:
             data=self.STREAM.read(self.CHUNK, exception_on_overflow=False)
@@ -85,31 +89,39 @@ class DOA_2D_listener():
             volume=audioop.rms(data,2)
             if(volume>self.MIN_VOLUME):
                 print('loud sound detected')
-                angle=self.Mic_tuning.direction
+                self.angle=self.Mic_tuning.direction
                 if i>int(frame_len*self.SOUND_PRE_OFFSET):
-                    test_frames[:int(frame_len*self.SOUND_PRE_OFFSET)]=frames[i-int(frame_len*self.SOUND_PRE_OFFSET):i]
+                    self.test_frames[:int(frame_len*self.SOUND_PRE_OFFSET)]=frames[i-int(frame_len*self.SOUND_PRE_OFFSET):i]
                 else:
-                    test_frames[:i]=frames[:i]
-                    test_frames[i:int(frame_len*self.SOUND_PRE_OFFSET)]=frames[int(frame_len*self.SOUND_PRE_OFFSET)+i:]
+                    self.test_frames[:i]=frames[:i]
+                    self.test_frames[i:int(frame_len*self.SOUND_PRE_OFFSET)]=frames[int(frame_len*self.SOUND_PRE_OFFSET)+i:]
                 for j in range(int(frame_len*self.SOUND_PRE_OFFSET),frame_len):
                     data=self.STREAM.read(self.CHUNK, exception_on_overflow=False)
-                    test_frames[j]=np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS)
+                    self.test_frames[j]=np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS)
+                if self.Mic_tuning.is_voice():
+                    print('voice detected, skip')
+                    i=0
+                    continue
+                # 다른 스레드에서 분석시작
                 print('record done, start callback function')
-                self.DETECT_CALLBACK(test_frames, angle)
-                i=0
-            i = i+1
-            if(i>=frame_len):
-                i=0
+                for th in self.CALLBACK_THREAD_LIST:
+                    if not th.is_alive():
+                        th.start()
+                        break
+                    if th is self.CALLBACK_THREAD_LIST[-1]:
+                        print('thread is full, ignore')
+            # i++
+            i = i+1%frame_len
     
-    def default_callback(self, test_frames, angle):
+    def default_callback(self):
         #실수화(librosa는 실수값으로 작동)
         #0번채널만 추출
-        test_frames_np_float = soundDataToFloat(np.array(test_frames)[:,:,0]).flatten()
+        test_frames_np_float = soundDataToFloat(np.array(self.test_frames)[:,:,0]).flatten()
         #모델에 넣기위한 작업과정
         feat = mfcc.pre_progressing(test_frames_np_float, self.RATE)
         result = self.MODEL.test_by_feat(feat)
         print(result)
-        print(angle)
+        print(self.angle)
         return
     
     def stop(self):
