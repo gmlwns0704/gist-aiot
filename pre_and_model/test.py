f5716a91d09259514667d45ed53f4d0af84b7fd8
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 08:29:07 +0000

    test model

--- pre_and_model/test.py
+++ pre_and_model/test.py
@@ -1,6 +1,10 @@
 import mfcc
 import model
 
+print('start gen model')
 model = model.Rasp_Model()
+print('start mfcc')
 mfcc.pre_progressing(input('filename: '))
+print('start model eval')
 model.test_from_image('sample.jpg')
+print('done')
