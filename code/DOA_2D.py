1eb3c9e82f04289ac0b303689d3bb67fa2ef31e8
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 10:52:55 +0000

    test doa

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -17,7 +17,7 @@ import pre_and_model.mfcc as mfcc
 import pre_and_model.model as model
 
 # 설정
-FORMAT = pyaudio.paInt16
+FORMAT = pyaudio.paFloat32
 CHANNELS = 6  # ReSpeaker v2.0은 6개의 채널을 지원합니다
 RATE = 16000  # 샘플 레이트
 CHUNK = 1024  # 버퍼 크기
@@ -82,24 +82,15 @@ while True:
         i=0
 
 print("* recorded")
-print(len(test_frames))
-print()
 
 # 스트림 종료
 stream.stop_stream()
 stream.close()
 p.terminate()
 
-print(np.array(test_frames).shape)
-
 #실수화(librosa는 실수값으로 작동)
-test_frames_np_int = np.array(test_frames[:][:][0], dtype=np.int16).flatten()
-print(test_frames_np_int)
-print(test_frames_np_int.shape)
-# 2. float32로 변환하고 -1.0 ~ 1.0 범위로 정규화
-test_frames_np_float = test_frames_np_int.astype(np.float32) / 32768.0
-
+test_frames_np = np.array(test_frames[:][:][0], dtype=np.int16).flatten()
 #모델에 넣기위한 작업과정
-feat = mfcc.pre_progressing(test_frames_np_float, RATE)
+feat = mfcc.pre_progressing(test_frames_np, RATE)
 result = rasp_model.test_by_feat(feat)
 print(result)
