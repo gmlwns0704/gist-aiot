7bdb629601d52b855e9e5fa09c20ba95b1040021
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 12:27:38 +0000

    tuning.sh

--- tuning_set.sh
+++ tuning_set.sh
@@ -1,4 +1,4 @@
 python tuning.py HPFONOFF 2
-python tuning.py STATIONNOISEONOFF 1
-python tuning.py NONSTATIONNOISEONOFF 0
+python tuning.py STATNOISEONOFF 1
+python tuning.py NONSTATNOISEONOFF 0
 python tuning.py AGCONOFF 0
