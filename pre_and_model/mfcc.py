37b32cca3ca54d49d551caafe3051e9387ba27ed
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 08:46:31 +0000

    test model

--- pre_and_model/mfcc.py
+++ pre_and_model/mfcc.py
@@ -2,16 +2,18 @@
 from PIL import Image
 import librosa
 import numpy as np
-def pre_progressing(filename, size=[32,32]):
+def pre_progressing_file(filename, size=[32,32]):
     padding = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.shape[0], i-a.shape[1]))))
 
     if '.wav' not in filename:
         print('not .wav file')
         return
     y, sr = librosa.load(filename, sr=16000)
+    print('file loaded')
     # 이미지 ndarray
     feat = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=100, n_fft=400, hop_length=160)
     # 패딩된 ndarray
+    print('mfcc done')
     feat = padding(feat, 500)
 
     # 해당 이미지 새로 저장
@@ -21,3 +23,4 @@ def pre_progressing(filename, size=[32,32]):
     # resize크기 적당히 조절
     img = img.resize(size)
     img.save('sample.jpg','JPEG')
+    print('save done')
--- pre_and_model/test.py
+++ pre_and_model/test.py
@@ -4,7 +4,7 @@ import model
 print('start gen model')
 model = model.Rasp_Model()
 print('start mfcc')
-mfcc.pre_progressing('/home/rasp/venv/gist-aiot/output_channel_1.wav')
+mfcc.pre_progressing_file('/home/rasp/venv/gist-aiot/code/output_channel_1.wav')
 print('start model eval')
 model.test_from_image('sample.jpg')
 print('done')
