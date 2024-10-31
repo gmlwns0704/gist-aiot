8c3f20bfb73af0518afbf7639cbbb8d6171c80a2
gmlwns0704 <gmlwns0704@gmail.com>
2024-10-31 15:14:16 +0000

    test

--- code/DOA_3D.py
+++ code/DOA_3D.py
@@ -12,18 +12,17 @@ mic_positions = np.array([
 ]).T
 
 # 샘플 WAV 파일 읽기 (다중 채널 WAV 파일이어야 합니다)
-fs, audio_data = read('input.wav')
+fs, audio_data = read('output_selected_channels.wav')
 
 # 입력 데이터 형식 변환 (samples, channels) -> (channels, samples)
 audio_data = audio_data.T
 
 # MUSIC 알고리즘을 사용하여 DOA 추정
 nfft = 256  # FFT 길이
-freq_bins = np.array([20, 40, 60])  # 분석할 주파수 빈 (예시)
 doa = pra.doa.music.MUSIC(mic_positions, fs, nfft, c=343)
 
 # DOA 계산
-doa.locate_sources(audio_data, freq_bins=freq_bins)
+doa.locate_sources(audio_data)
 
 # 추정된 방향 (azimuth angles in degrees)
 azimuths = doa.azimuth_recon
