500eca1f83ee3f51d57039ba7ae100646075f7b2
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-02 09:19:27 +0000

    3d test

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -26,7 +26,7 @@ def soundDataToFloat(SD):
 
 class DOA_2D_listener():
     def __init__(self,
-                 channels=6,
+                 channels=4,
                  sr=16000,
                  chunk=1024,
                  record_seconds=3,
@@ -34,7 +34,7 @@ class DOA_2D_listener():
                  sound_pre_offset=0.3,
                  input_model=None):
         self.FORMAT = pyaudio.paInt16
-        self.CHANNELS = channels
+        self.RESP_CHANNELS = 6
         self.RATE = sr
         self.CHUNK = chunk
         self.RECORD_SECONDS = record_seconds
@@ -57,36 +57,50 @@ class DOA_2D_listener():
             print('device not found')
             return None
         self.Mic_tuning=Tuning(self.dev)
+        
+        device_index = None
+        for i in range(self.pyaudio_instance.get_device_count()):
+            dev = self.pyaudio_instance.get_device_info_by_index(i)
+            name = dev['name'].encode('utf-8')
+            print('{}:{} with {} input channels'.format(i, name, dev['maxInputChannels']))
+            if name.find('ReSpeaker 4 Mic Array') >= 0 and dev['maxInputChannels'] == self.channels:
+                device_index = i
+                break
 
         # 입력 스트림 설정
         self.STREAM = self.PYAUDIO_INSTANCE.open(format=self.FORMAT,
-                        channels=self.CHANNELS,
+                        channels=self.RESP_CHANNELS,
                         rate=self.RATE,
                         input=True,
-                        frames_per_buffer=self.CHUNK)
+                        frames_per_buffer=self.CHUNK,
+                        input_device_index=device_index)
+    
+    #0-5채널 전부 읽음
+    def read_stream(self):
+        data = self.STREAM.read(self.CHUNK, exception_on_overflow=False)
+        return np.frombuffer(data, dtype=np.int16).reshape(-1, self.RESP_CHANNELS)
     
     def start_detect(self):
         frames = []
         self.test_frames=[]
-        print(self.RATE)
-        print(self.CHUNK)
-        print(self.RECORD_SECONDS)
+        # print(self.RATE)
+        # print(self.CHUNK)
+        # print(self.RECORD_SECONDS)
         frame_len=int((self.RATE/self.CHUNK)*self.RECORD_SECONDS)
         for i in range(frame_len):
-            data = self.STREAM.read(self.CHUNK, exception_on_overflow=False)
-            frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS))
-            self.test_frames.append(np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS))
+            frame = self.read_stream()
+            frames.append(frame)
+            self.test_frames.append(frame)
 
         print("* waiting for loud volume")
         i=0
         while True:
-            data=self.STREAM.read(self.CHUNK, exception_on_overflow=False)
-            frames[i]=np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS)
+            frames[i]=self.read_stream()
             # data, 각 2byte
             volume=audioop.rms(frames[i][:,0].flatten(),2)
             if(volume>self.MIN_VOLUME):
                 print('loud sound detected')
-                self.angle=self.Mic_tuning.direction
+                # self.angle=self.Mic_tuning.direction
                 x=int(frame_len*self.SOUND_PRE_OFFSET)
                 if i>x:
                     self.test_frames[:x]=frames[i-x:i]
@@ -94,14 +108,10 @@ class DOA_2D_listener():
                     self.test_frames[:x-i]=frames[frame_len-(x-i):]
                     self.test_frames[x-i:x]=frames[:i]
                 for j in range(x,frame_len):
-                    data=self.STREAM.read(self.CHUNK, exception_on_overflow=False)
-                    self.test_frames[j]=np.frombuffer(data, dtype=np.int16).reshape(-1, self.CHANNELS)
-                # 다른 스레드에서 분석시작
+                    self.test_frames[j]=self.read_stream()
+                # 분석시작
                 print('record done, start callback function')
-                # th=threading.Thread(target=self.default_callback, args=(self.test_frames,))
-                # th.start()
                 self.default_callback(self.test_frames)
-                print('thread call done')
             # i++
             i = (i+1)%frame_len
     
@@ -113,7 +123,7 @@ class DOA_2D_listener():
         feat = mfcc.pre_progressing(test_frames_np_float, self.RATE)
         result = self.MODEL.test_by_feat(feat)
         print(result)
-        print(self.angle)
+        # print(self.angle)
         return
     
     def stop(self):
@@ -153,12 +163,15 @@ class DOA_pra_listener(DOA_2D_listener):
                     [0.035, 0]
                 ]).T
         if dim == 3:
-            self.mic_3d_positions_x = np.array([
-                [0.035,0],
-                [-0.035,0],
-                [0,0.035]
-            ])
-            self.mic_3d_positions_y = np.array([
+            # 3D용 보조 마이크 인스턴스
+            self.PYAUDIO_INSTANCE_DIM3 = pyaudio.PyAudio()
+            self.STREAM_DIM3 = self.PYAUDIO_INSTANCE.open(format=self.FORMAT,
+                        channels=1,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+            
+            self.mic_3d_positions = np.array([
                 [0.035,0],
                 [-0.035,0],
                 [0,0.035]
@@ -174,18 +187,24 @@ class DOA_pra_listener(DOA_2D_listener):
                                      c=343)
         
         if dim == 3:
-            self.doa_3d_x=pra.doa.music.MUSIC(self.mic_3d_positions_x,
+            self.doa_3d=pra.doa.music.MUSIC(self.mic_3d_positions,
                                         self.RATE,
                                         nfft=self.nfft,
                                         c=343)
-            
-            self.doa_3d_y=pra.doa.music.MUSIC(self.mic_3d_positions_y,
-                                        self.RATE,
-                                        nfft=self.nfft,
-                                        c=343)
+        return
+    
+    #0-5채널+6채널(보조마이크) 추가
+    def read_stream(self):
+        data = super().read_stream()
+        if self.dim == 3:
+            data_3d = np.frombuffer(self.STREAM_DIM3.read(self.CHUNK, exception_on_overflow=False), dtype=np.int16).reshape(-1, 1)
+            return np.vstack((data, data_3d)).shape
+        else:
+            return data
     
     def default_callback(self, input_test_frames):
         test_frames_np=np.array(input_test_frames)[:,:,1:5]
+        test_frames_np_3d=np.array(input_test_frames)[:,:,]
         print(test_frames_np.shape)
         X = np.array(
             [
@@ -194,9 +213,31 @@ class DOA_pra_listener(DOA_2D_listener):
             ]
         )
         self.doa.locate_sources(X)
-        print(f"Estimated DOA angles: {self.doa.azimuth_recon / np.pi * 180.0} degrees")
+        h_angle = self.doa.azimuth_recon
+        print(f"Estimated DOA angles: {h_angle / np.pi * 180.0} degrees")
+        
         if self.dim == 3:
-            print(f"Estimated DOA elev angles: {self.doa.colatitude_recon / np.pi * 180.0} degrees")
+            X_3D_y = np.array(
+                [
+                    pra.transform.stft.analysis(test_frames_np[:,:,ch].flatten(), self.nfft, self.nfft // 2).T
+                    for ch in [1,3,6]
+                ]
+            )
+            X_3D_x = np.array(
+                [
+                    pra.transform.stft.analysis(test_frames_np[:,:,ch].flatten(), self.nfft, self.nfft // 2).T
+                    for ch in [2,4,6]
+                ]
+            )
+            self.doa_3d.locate_source(X_3D_x)
+            v_angle_x = self.doa_3d.azimuth_recon
+            self.doa_3d.locate_source(X_3D_y)
+            v_angle_y = self.doa_3d.azimuth_recon
+            print(f"Estimated DOA v_x angles: {v_angle_x / np.pi * 180.0} degrees")
+            print(f"Estimated DOA v_y angles: {v_angle_y / np.pi * 180.0} degrees")
+            v_angle = ((np.cos(h_angle)**2)*v_angle_x + (np.sin(h_angle)**2)*v_angle_y)
+            print(f"Estimated DOA v angles: {v_angle / np.pi * 180.0} degrees")
+        
         return super().default_callback(input_test_frames)
 
 class DOA_TDOA_listener(DOA_2D_listener):
