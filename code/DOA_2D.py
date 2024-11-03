f2822323e0166fdd8d278631bd28307e0385778d
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-03 12:51:59 +0000

    test

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -272,8 +272,8 @@ class DOA_pra_listener(DOA_2D_listener):
         
         # 감지되었다면 test_frames도 같이 업데이트
         if self.detected:
-            self.test_frames[self.chunk_count][:,4] = np_data[:,1]
-        self.chunks[self.chunk_count][:,4] = np_data[:,1]
+            self.test_frames[self.chunk_count][:,4] = np_data[:]
+        self.chunks[self.chunk_count][:,4] = np_data[:]
         self.chunk_count += 1
         
         # 루프사이클
