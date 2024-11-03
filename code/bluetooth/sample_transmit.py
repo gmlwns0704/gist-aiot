b23ab15537eb8f1a796270238731c8e730b36eeb
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-03 17:48:24 +0000

    test bt

--- code/bluetooth/sample_transmit.py
+++ code/bluetooth/sample_transmit.py
@@ -1,27 +1,32 @@
-# http://www.makeshare.org/bbs/board.php?bo_table=raspberrypi&wr_id=490
+import asyncio
+from bleak import BleakScanner, BleakClient
 
-import bluetooth
-server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-port = 1
-server_socket.bind(("",port))
-server_socket.listen(1)
+# 스마트폰에서 광고하는 장치의 MAC 주소와 서비스 UUID
+TARGET_MAC_ADDRESS = "xx:xx:xx:xx:xx:xx"  # 스마트폰의 BLE 주소로 변경
+TARGET_SERVICE_UUID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"  # 전송하고자 하는 서비스 UUID
 
+async def run():
+    # 장치 스캔
+    print("Scanning for BLE devices...")
+    devices = await BleakScanner.discover()
+    for device in devices:
+        print(device)
 
-client_socket,address = server_socket.accept()
+    # 대상 장치 찾기
+    device = next((d for d in devices if d.address == TARGET_MAC_ADDRESS), None)
+    if device is None:
+        print("Target device not found.")
+        return
 
-print("Accepted connection from ",address)
+    print("Connecting to", TARGET_MAC_ADDRESS)
+    async with BleakClient(device) as client:
+        print("Connected to device")
 
-while 1:
+        # 서비스에서 데이터를 수신
+        while True:
+            data = await client.read_gatt_char(TARGET_SERVICE_UUID)
+            print("Received:", data.decode("utf-8"))
+            await asyncio.sleep(1)
 
-    data = client_socket.recv(1024)
-    print("Received: %s" % data)
-    if (data == "q"):
-        print ("Quit")
-    
-    break
-
- 
-
-client_socket.close()
-
-server_socket.close()
+# 비동기 실행
+asyncio.run(run())
