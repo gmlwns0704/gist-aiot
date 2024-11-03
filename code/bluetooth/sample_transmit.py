7c41cb15b89b73fb172e49f5a27451ce062d3849
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-03 18:31:31 +0000

    test bt

--- code/bluetooth/sample_transmit.py
+++ code/bluetooth/sample_transmit.py
@@ -1,45 +1,46 @@
-import bluetooth
+from bluetooth import *
  
-#######################################################
-# Scan
-#######################################################
+def receiveMsg():
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
  
-target_name = "iot_test"   # target device name
-target_address = None
-port = 1         # RFCOMM port
+    # RFCOMM 포트를 통해 데이터 통신을 하기 위한 준비    
+    server_sock=BluetoothSocket( RFCOMM )
+    server_sock.bind(('',PORT_ANY))
+    server_sock.listen(1)
  
-nearby_devices = bluetooth.discover_devices()
+    port = server_sock.getsockname()[1]
  
-# scanning for target device
-for bdaddr in nearby_devices:
-    print(bluetooth.lookup_name( bdaddr ))
-    if target_name == bluetooth.lookup_name( bdaddr ):
-        target_address = bdaddr
-        break
- 
-if target_address is not None:
-    print('device found. target address %s' % target_address)
-else:
-    print('could not find target bluetooth device nearby')
- 
-#######################################################
-# Connect
-#######################################################
- 
-# establishing a bluetooth connection
-try:
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((target_address, port))
- 
-    while True:         
+    # 블루투스 서비스를 Advertise
+    advertise_service( server_sock, "BtChat",
+            service_id = uuid,
+            service_classes = [ uuid, SERIAL_PORT_CLASS ],
+            profiles = [ SERIAL_PORT_PROFILE ] )
+    
+    print("Waiting for connection : channel %d" % port)
+    # 클라이언트가 연결될 때까지 대기
+    client_sock, client_info = server_sock.accept()
+    print('accepted')
+    while True:          
+        print("Accepted connection from ", client_info)
         try:
-            recv_data = sock.recv(1024)
-            print(recv_data)
-            sock.send(recv_data)
+            # 들어온 데이터를 역순으로 뒤집어 전달
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            print("received [%s]" % data)
+            print("send [%s]" % data[::-1])
+            client_sock.send(data[::-1])
+        except IOError:
+            print("disconnected")
+            client_sock.close()
+            server_sock.close()
+            print("all done")
+            break
+ 
         except KeyboardInterrupt:
             print("disconnected")
-            sock.close()
+            client_sock.close()
+            server_sock.close()
             print("all done")
-except bluetooth.btcommon.BluetoothError as err:
-    print('An error occurred : %s ' % err)
-    pass
+            break
+ 
+receiveMsg()
