9c3e1f82df668f5e27b235da13c26a515c44414b
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-04 08:25:37 +0000

    test

--- code/DOA_start.py
+++ code/DOA_start.py
@@ -3,10 +3,15 @@ import numpy as np
 import pyroomacoustics as pra
 import sys
 
+from bt import bt_transmit
+
+bt_class = bt_transmit.bt_communicate()
+
 listener = DOA_2D.DOA_pra_listener(
         record_seconds=int(sys.argv[1]),
         sound_pre_offset=float(sys.argv[2]),
-        min_volume=int(sys.argv[3])
+        min_volume=int(sys.argv[3]),
+        bt_class=bt_class
         )
 
 listener.start_detect()
