1a5570ba457019699cc35ce5d7e76bd12625e4c1
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-06 09:58:42 +0000

    test

--- pre_and_model/test.py
+++ pre_and_model/test.py
@@ -4,7 +4,7 @@ import model
 print('start gen model')
 model = model.Rasp_Model()
 print('start mfcc')
-mfcc.pre_progressing_file('/home/rasp/venv/gist-aiot/code/output_channel_1.wav')
+mfcc.pre_progressing_file('/home/rasp/venv/gist-aiot/code/output_channel_2.wav')
 print('start model eval')
 print(model.test_from_image())
 print('done')
