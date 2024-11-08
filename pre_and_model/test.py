200cba2797bb789f7f2508a3a5817c93c2f37c1c
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-08 01:58:48 +0000

    t

--- pre_and_model/test.py
+++ pre_and_model/test.py
@@ -1,5 +1,6 @@
 import mfcc
 import model
+import numpy as np
 
 print('start gen model')
 model = model.Rasp_Model()
@@ -7,5 +8,5 @@ print('start mfcc')
 mfcc.pre_progressing_file('./sample.wav')
 print('start model eval')
 result = model.test_from_image()
-print(10**result)
+print(np.exp(result))
 print('done')
