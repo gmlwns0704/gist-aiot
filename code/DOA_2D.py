1bb1f4f5db8ea2807416a0a97e936615f5e96405
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-08 07:47:36 +0000

    t

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -174,9 +174,9 @@ class DOA_2D_listener():
                         # print(self.multi_frames_reult_class[i])
                         # print(self.multi_frames_reult_value[i])
                         if self.bt_class is not None:
-                            # print(self.multi_frames_angle[i])
-                            # print(self.multi_frames_reult_class[i])
-                            # print(self.multi_frames_reult_value[i])
+                            print(self.multi_frames_angle[i])
+                            print(self.multi_frames_reult_class[i])
+                            print(self.multi_frames_reult_value[i])
                             if self.multi_frames_reult_value[i] > self.estimate_rate and self.multi_frames_reult_class[i] not in ignore_class:
                                 self.bt_buffer += 'angle:'+str(self.multi_frames_angle[i])+'\n'
                                 self.bt_buffer += 'class:'+str(self.multi_frames_reult_class[i])+'\n'
