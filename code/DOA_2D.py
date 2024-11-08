e284a53b7ea3181fefdbbfa5ce9571acc0a2b9bc
gmlwns0704 <gmlwns0704@gmail.com>
2024-11-08 05:56:18 +0000

    t

--- code/DOA_2D.py
+++ code/DOA_2D.py
@@ -143,8 +143,8 @@ class DOA_2D_listener():
     def start_detect(self):
         print("detection started")
         print('multithread started')
-        executor = concurrent.futures.ThreadPoolExecutor()
-        futures = [executor.submit(self.threading_detect_callback, i) for i in range(self.multi_frames_num)]
+        # executor = concurrent.futures.ThreadPoolExecutor()
+        # futures = [executor.submit(self.threading_detect_callback, i) for i in range(self.multi_frames_num)]
         while True:
             x_rot, y_rot = gyro.get_angle()
             if np.abs(x_rot) > 45 or np.abs(y_rot) > 45:
